aa7a9e481fc52930f4e03f12036ecd415bc6d966
neagpaul11@gmail.com <neagpaul11@gmail.com>
2022-10-12 00:50:27 +0300

    ORCA-907 Improvement: Bad coding style in javascript call

--- index.js
+++ index.js
@@ -6,7 +6,14 @@ import initI18n from "@/app/utils/init-i18n";
 import initLMSData from "@/app/utils/init-lms-data";
 import buildTreeArray from "@/app/utils/build-tree-array";
 
-const init = (tools, categories, translations, options, error) => {
+const init = () => {
+
+  const tools = (!categories)? document.getElementsByName('lti_selector_tools')[0].value : {};
+  const categories = (!categories)? document.getElementsByName('lti_selector_categories')[0].value : {};
+  const translations = (!translations)? document.getElementsByName('lti_selector_translations')[0].value : {};
+  const options = (!options)? document.getElementsByName('lti_selector_options')[0].value : {};
+  const error = (!error) ? document.getElementsByName('lti_selector_error')[0].value : '';
+
   const { parsedTools, parsedCategories, parsedTranslations, parsedOptions } =
     initLMSData(tools, categories, translations, options);
   const i18n = initI18n(parsedTranslations);
